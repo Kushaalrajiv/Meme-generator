9086d64aa187d4d5f6dda0f655973c9da79d2131
kushaal <kushaalrajiv@gmail.com>
2023-12-04 13:12:42 +0530

    third commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,18 +26,23 @@ export default function Main(){
         })
     }
     return (
-        <main className="main">
-            {/* <form className="form"> */}
-                <input type="text" placeholder="Top Text" className="form--text" name="topText" value={meme.topText} onChange={handleChange}/>
-                <input type="text" placeholder="Bottom Text" className="form--text" name="bottomText" value={meme.bottomText}onChange={handleChange}/>
-                <button className="form--button" onClick={getMemes}>Get a new meme image 🖼️</button>
+        <div>
+            <main className="main">
+           
+                    <input type="text" placeholder="Top Text" className="form--text" name="topText" value={meme.topText} onChange={handleChange}/>
+                    <input type="text" placeholder="Bottom Text" className="form--text" name="bottomText" value={meme.bottomText}onChange={handleChange}/>
+                    <button className="form--button" onClick={getMemes}>Get a new meme image 🖼️</button>
+                </main>
                 <div className="meme">
-                <img src={meme.randomImage} className="form--image"/>
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
-            </div>
-                
-            {/* </form> */}
-        </main>
+                    <img src={meme.randomImage} className="form--image"/>
+
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                    </div>
+                    
+                    
+            
+        
+        </div>
     )
 }
